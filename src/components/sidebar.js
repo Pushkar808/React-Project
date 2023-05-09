9d2a85ba2d1e41ac8276b0d5d3bb77d5f895f204
pushkar gupta <pushkargupta@youngangels.local>
2023-05-09 16:53:29 +0530

    first commit

---
+++ src/components/sidebar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function Sidebar() {
+    return (
+        <div className='w-1/3 border-2 p-4 dark:bg-slate-500' >
+            <div class="relative">
+                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                        fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z">
+                        </path>
+                    </svg>
+                </div>
+                <input type="search" id="default-search"
+                    class="block w-full p-4 pl-10 text-sm text-black border border-gray-300 rounded-lg bg-gray-50  dark:bg-gray-600 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Search for conversations"
+                />
+            </div>
+            <div className='py-4 flex text-sm dark:text-white justify-between border-b-[1.5px] border-gray-500 dark:border-slate-300'>
+                <div className='p-2'> CONVERSATIONS</div>
+                <div className='rounded-full p-2'>+</div>
+            </div>
+            <div className='py-4'>
+            
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
---
+++ src/components/sidebarChat.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function sidebarChat() {
+  return (
+    <div>sidebarChat</div>
+  )
+}
+
+export default sidebarChat
