2b980387b44658899578060811951b1d64b4788d
pushkar gupta <pushkargupta@youngangels.local>
2023-05-16 17:48:07 +0530

    some bug fixes

--- src/components/mainChat.js
+++ src/components/mainChat.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SendChat from './sendChat';
 import RecieveChat from './recieveChat';
-
+import MessageData from '../data/messages.json'
 function Mainchat() {
+    const [message, setmessage] = useState("");
+    const sendMessage = () => {
+        
+    }
     return (
         <>
             <div className='w-full flex flex-col border-2 border-red-400 p-4 justify-between'>
@@ -11,8 +15,8 @@ function Mainchat() {
                     <RecieveChat message={"Recieve chat"} />
                 </div>
                 <div className='relative'>
-                    <input type='text' className='w-full p-4 pr-16 bg-gray-100' placeholder='Type your message here.' />
-                    <button className='absolute right-0 p-4 hover:bg-gray-300'>Send</button>
+                    <input type='text' className='w-full p-4 pr-16 bg-gray-100' placeholder='Type your message here.' onChange={(element) => { console.log(element.target.value) }} />
+                    <button className='absolute right-0 p-4 hover:bg-gray-300' onClick={sendMessage}>Send</button>
                 </div>
             </div>
         </>
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -5,17 +5,17 @@ function Sidebar() {
     console.log()
     return (
         <div className='w-1/3 h-screen border-2 p-4 dark:bg-slate-800 overflow-x-auto' >
-            <div class="relative">
-                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                    <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400"
+            <div className="relative">
+                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400"
                         fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                             d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z">
                         </path>
                     </svg>
                 </div>
                 <input type="search" id="default-search"
-                    class="block w-full p-4 pl-10 text-sm text-black border border-gray-300 rounded-lg bg-gray-50  dark:bg-gray-600 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="block w-full p-4 pl-10 text-sm text-black border border-gray-300 rounded-lg bg-gray-50  dark:bg-gray-600 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Search for conversations"
                 />
             </div>
@@ -26,9 +26,9 @@ function Sidebar() {
 
             <div className=''>
                 {
-                    JsonData.contactinfo.map((element) => {
+                    JsonData.contactinfo.map((element, index) => {
                         return (
-                            <SidebarChat name={element.Name} message={"element.Contact"} />)
+                            <SidebarChat name={element.Name} key={index} message={"element.Contact"} />)
                     })
                 }
             </div>
